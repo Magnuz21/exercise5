28eedc11a6ed1c09ef979379073f7327d74e8419
Aldrin Chicote Abilar <101628771+Magnuz21@users.noreply.github.com>
2023-04-14 19:32:08 -0700

    Update display_card.js

--- src/components/display_card.js
+++ src/components/display_card.js
@@ -6,17 +6,6 @@ import { useRouter } from "next/router";
 
 
 
-// EXERCISE 3 - 5 
-//  S
-//  U
-//  B
-//  M
-//  I
-//  T
-//  T
-//  E
-//  D BY " J E R S O N  R A Y B. D E S I E R T O "
-
 
 
 
